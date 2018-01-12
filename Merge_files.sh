ab227f31e677e9890b84e6b42bba46a7d960bb9a
ArnauVich <arnauvich@gmail.com>
2018-01-12 16:12:20 +0100

    fix

--- Merge_files.sh
+++ Merge_files.sh
@@ -3,4 +3,4 @@
 #Usage: Command line
 #Combines two files using the first column of each file
 
-awk ' FNR==NR { a[$1]=$1; next } $1 in a { print a[$1] “\t” $0 }'  $index.txt $INPUT.txt  > $OUTPUT.txt
+awk ' FNR==NR { a[$1]=$1; next } $1 in a { print a[$1] "\t" $0 }'  $index.txt $INPUT.txt  > $OUTPUT.txt
